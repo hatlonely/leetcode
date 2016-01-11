6114d3797ddf1d53396a98a26cf2e730c7de66e6
hatlonely <hatlonely@foxmail.com>
2016-01-11 14:43:53 +0800

    add 6_zigzag_conversion.cpp

---
+++ cpp/leetcode/leetcode/6_zigzag_conversion.cpp
@@ -0,0 +1,91 @@
+//
+//  6_ zigzag_conversion.cpp
+//  leetcode
+//
+//  Created by hatlonely on 16/1/11.
+//  Copyright © 2016年 hatlonely. All rights reserved.
+//
+//  The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+//  (you may want to display this pattern in a fixed font for better legibility)
+//
+//  P   A   H   N
+//  A P L S I I G
+//  Y   I   R
+//  And then read line by line: "PAHNAPLSIIGYIR"
+//  Write the code that will take a string and make this conversion given a number of rows:
+//
+//  string convert(string text, int nRows);
+//  convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+//
+
+#include <iostream>
+#include <cassert>
+#include <string>
+
+namespace zigzag_conversion {
+
+class Solution {
+public:
+    std::string convert(std::string str, int n) {
+        int len = (int)str.length();
+        std::string result;
+        result.reserve(str.length());
+        
+        if (n == 1) {
+            return str;
+        }
+        
+        int mod = (n - 1) * 2;
+        {
+            int i = 0;
+            while (true) {
+                int idx = (i++) * mod;
+                if (idx >= len) {
+                    break;
+                }
+                result += str[idx];
+            }
+        }
+        
+        for (int count = 1; count < n - 1; count++) {
+            int i = 0;
+            while (true) {
+                int idx1 = i * mod + count;
+                if (idx1 >= len) {
+                    break;
+                }
+                result += str[idx1];
+                int idx2 = i * mod + mod - count;
+                if (idx2 >= len) {
+                    break;
+                }
+                result += str[idx2];
+                i++;
+            }
+        }
+        
+        {
+            int i = 0;
+            while (true) {
+                int idx = (i++) * mod + n - 1;
+                if (idx >= len) {
+                    break;
+                }
+                result += str[idx];
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main(int argc, const char *argv[]) {
+    Solution solution;
+    
+    assert(solution.convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
+    assert(solution.convert("A", 1) == "A");
+    
+    return 0;
+}
+
+}
--- cpp/leetcode/leetcode/main.cpp
+++ cpp/leetcode/leetcode/main.cpp
@@ -13,7 +13,8 @@ MAIN_NAMESPACE(add_two_numbers)
 MAIN_NAMESPACE(longest_substring_without_repeating_characters)
 MAIN_NAMESPACE(median_of_two_sorted_arrays)
 MAIN_NAMESPACE(longest_palindromic_substring)
+MAIN_NAMESPACE(zigzag_conversion)
 
 int main(int argc, const char *argv[]) {
-    return longest_palindromic_substring::main(argc, argv);
+    return zigzag_conversion::main(argc, argv);
 }
