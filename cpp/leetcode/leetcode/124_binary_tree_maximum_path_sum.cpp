36a5266e593d12917bcc1b350c8cdacd1c3de45f
hatlonely <hatlonely@gmail.com>
2016-01-26 19:36:13 +0800

    add binary_tree_maximum_path_sum

---
+++ cpp/leetcode/leetcode/124_binary_tree_maximum_path_sum.cpp
@@ -0,0 +1,73 @@
+//
+//  124_binary_tree_maximum_path_sum.cpp
+//  leetcode
+//
+//  Created by hatlonely on 16/1/26.
+//  Copyright © 2016年 hatlonely. All rights reserved.
+//
+//  Given a binary tree, find the maximum path sum.
+//
+//  For this problem, a path is defined as any sequence of nodes from some starting
+//  node to any node in the tree along the parent-child connections.
+//  The path does not need to go through the root.
+//
+//  For example:
+//  Given the below binary tree,
+//
+//         1
+//        / \
+//       2   3
+//  Return 6.
+//
+
+#include <iostream>
+#include <cassert>
+#include "tree_node.h"
+
+namespace binary_tree_maximum_path_sum {
+    
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        long max = INT_MIN;
+        max_path_sum_process(root, max);
+        return (int)max;
+    }
+    
+    long max_path_sum_process(TreeNode *root, long &max) {
+        if (root == nullptr) {
+            return 0;
+        }
+        long lmax = max_path_sum_process(root->left, max);
+        long rmax = max_path_sum_process(root->right, max);
+        
+        if (lmax < 0) {
+            lmax = 0;
+        }
+        if (rmax < 0) {
+            rmax = 0;
+        }
+        max = std::max(max, lmax + rmax + root->val);
+        
+        return std::max(lmax, rmax) + root->val;
+    }
+};
+
+int main(int argc, const char *argv[]) {
+    auto test = [](std::initializer_list<int> li, int expected) {
+        Solution solution;
+        TreeNode *root = CreateTree(li);
+        int result = solution.maxPathSum(root);
+        DestroyTree(root);
+        std::cout << result << std::endl;
+        return result == expected;
+    };
+    
+    test({1, 2, 3}, 6);
+    test({4, -2, -4}, 4);
+    test({4, -2}, 4);
+    
+    return 0;
+}
+    
+}
--- cpp/leetcode/leetcode/main.cpp
+++ cpp/leetcode/leetcode/main.cpp
@@ -131,9 +131,10 @@ MAIN_NAMESPACE(triangle)
 MAIN_NAMESPACE(best_time_to_buy_and_sell_stock)
 MAIN_NAMESPACE(best_time_to_buy_and_sell_stock_ii)
 MAIN_NAMESPACE(best_time_to_buy_and_sell_stock_iii)
+MAIN_NAMESPACE(binary_tree_maximum_path_sum)
 
 int main(int argc, const char *argv[]) {
-    return best_time_to_buy_and_sell_stock_iii::main(argc, argv);
+    return binary_tree_maximum_path_sum::main(argc, argv);
 }
 
 
